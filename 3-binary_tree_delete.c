dbfbd993ab3d86925b74bc202a04040666de806c
Samuel <a2jsamuel@gmail.com>
2023-05-30 23:46:03 +0300

    add 3-binary file for task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -11,7 +11,8 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree == NULL)
 		return;
 
-	else {
+	else
+	{
 		binary_tree_delete(tree->left);
 		binary_tree_delete(tree->right);
 		free(tree);
